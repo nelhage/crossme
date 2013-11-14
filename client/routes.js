badee8ac425ccfe360b3244deba74d928f8f9dfc
Nelson Elhage <nelhage@nelhage.com>
2013-11-13 22:40:14 -0800

    Make the CrossMe button go to /

--- client/routes.js
+++ client/routes.js
@@ -1,4 +1,8 @@
 Meteor.Router.add({
+  '/': { as: 'root', and: function (id) {
+    Session.set('gameid', null);
+    Session.set('previewid', null);
+  }},
   '/game/:id': {as: 'game', and: function (id) {
     Session.set('gameid', id);
     Session.set('previewid', null);
