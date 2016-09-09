a85e6d3acc103d5fad0569d2e60baa5517da4b05
Nelson Elhage <nelhage@nelhage.com>
2016-09-08 18:11:01 -0700

    Update for Meteor 1.4.x

--- packages/npm-deps/package.js
+++ packages/npm-deps/package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: "CrossMe npm dependencies"
 });
 
-Npm.depends({"iconv": "2.0.6"});
+Npm.depends({"iconv": "2.2.1"});
 
 Package.on_use(function (api, where) {
   api.add_files('index.js', ['server']);
