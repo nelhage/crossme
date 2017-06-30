e6433377ccbe4cc311e2f252a28a48c9628690a9
Nelson Elhage <nelhage@nelhage.com>
2017-06-29 21:15:08 -0700

    dead code

--- client/routes.js
+++ client/routes.js
@@ -20,22 +20,3 @@ Router.route('/preview/:id', function() {
 }, {
   name: 'preview',
 });
-
-/*
-Meteor.Router.add({
-  '/': { as: 'root', and: function (id) {
-    Session.set('gameid', null);
-    Session.set('previewid', null);
-  }},
-  '/game/:id': {as: 'game', and: function (id) {
-    Session.set('gameid', id);
-    Session.set('previewid', null);
-    return null;
-  }},
-  '/preview/:id': {as: 'preview', and: function (id) {
-    Session.set('gameid', null);
-    Session.set('previewid', id);
-    return null;
-  }}
-});
-*/
