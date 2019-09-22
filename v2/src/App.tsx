750ffb18d272430ed4210bf69b19f757e4446ff6
Nelson Elhage <nelhage@nelhage.com>
2019-09-22 14:02:07 -0700

    no logo

--- v2/src/App.tsx
+++ v2/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
