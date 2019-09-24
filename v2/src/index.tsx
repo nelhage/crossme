44535d43c2c31a2bcb09d3d46402ec54a8563ed7
Nelson Elhage <nelhage@nelhage.com>
2019-09-23 17:22:57 -0700

    prettier

--- v2/src/index.tsx
+++ v2/src/index.tsx
@@ -1,5 +1,5 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
